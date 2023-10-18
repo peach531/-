0ad3544f1dea56bda3cae94cf65cd280f54fef13
peach531 <128690701+peach531@users.noreply.github.com>
2023-10-18 18:46:33 +0900

    Add files via upload

---
+++ CD-p1(6)/2.py
@@ -0,0 +1,7 @@
+t1 = (1,)
+t2 = (4,5,6)
+print(t1 + t2)
+print(t2 *2)
+print(t2[0])
+print(t2[1:3])
+del t2[0]
---
+++ CD-p1(6)/3.py
@@ -0,0 +1,5 @@
+s1 = set([1,2,3,4,5,6])
+s2 = set([4,5,6,7,8,9])
+print(s1 - s2)
+print(s1 & s2)
+print(s1 | s2)
