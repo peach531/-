b54712d558b05fc8098d913bf5b47112a2fe8c15
peach531 <128690701+peach531@users.noreply.github.com>
2023-10-20 18:15:11 +0900

    Add files via upload

---
+++ qwertyuiop.py
@@ -0,0 +1,15 @@
+import turtle
+
+x = int(input('x:'))
+y = int(input('y:'))
+turtle.goto(x,y)
+x = int(input('x:'))
+y = int(input('y:'))
+turtle.goto(x,y)
+x = int(input('x:'))
+y = int(input('y:'))
+turtle.goto(x,y)
+x = int(input('x:'))
+y = int(input('y:'))
+turtle.goto(x,y)
+
