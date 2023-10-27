abcb6e5095a315a654c7af580530f6af6b470b26
peach531 <128690701+peach531@users.noreply.github.com>
2023-10-27 18:19:13 +0900

    Add files via upload

---
+++ sfsdfsdsfewfefdsfewvdewcxfewfbdafdaseeswqf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) 
+{
+	srand(time(NULL));
+	printf("%d",rand());
+}
