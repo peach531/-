94450384b03631b326c4d4fe2d49f65597c01222
peach531 <128690701+peach531@users.noreply.github.com>
2024-01-18 17:26:01 +0900

    Add files via upload

--- 6.py
+++ 6.py
@@ -1,5 +1,16 @@
-a = [10,20,30,40,50]
-a[0] = 'a'
-print(a)
-a[1:3] = 'b'
-print(a)
+class Human:
+    def __init__(self,name,age,sex):
+        self.name = name
+        self.age = age
+        self.sex = sex
+
+    def who(self):
+        print("이름: {}, 나이:{},성별:{},".format(self.name, self.age,self.sex))
+
+    def setInfo(self,name,age,sex):
+        self.name = name
+        self.age = age
+        self.sex = sex
+
+areum = Human("모름",0,"모름")
+areum.setInfo("아름",25,"여자")
--- 7.py
+++ 7.py
@@ -1,7 +1,16 @@
-a = 'I Love You'
-print(a.count('o'))
-print(a.find('Y'),a.index('Y'))
-print('.'.join(a))
-print(a.split())
-print(a.replace('Love', 'Like'))
-print(a.upper(),a.lower())
+s = int(input())
+b = int(input())
+p = int(input())
+B = 0
+S = 0
+
+b += p
+
+if b > 60:
+    B = b % 60
+    S = s + b / 60
+if S >= 24:
+    S = S - 24
+
+S = int(S)
+print(S,B)
